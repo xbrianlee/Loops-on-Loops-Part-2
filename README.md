925f7be026acbb6e76ebbfc12861734be41aa6c6
Laptopmini <laptopmini@gmail.com>
2015-03-02 21:25:10 -0800

    Quicksort in Haskell

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Loops-on-Loops-Part-2
+
+# Team Members:
+# - Paul-Valentin Mini
+# - Max Hufft
+# - John Kang
+# - Brian Lee
---
+++ quicksort.hs
@@ -0,0 +1,8 @@
+-- Quicksort in Haskell
+
+quicksort :: Ord a => [a] -> [a]
+quicksort []     = []
+quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
+    where
+        lesser  = filter (< p) xs
+        greater = filter (>= p) xs
