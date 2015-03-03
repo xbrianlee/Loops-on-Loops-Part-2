3ff6b2e6dd74403903bd7f737ff9fddf1ecd6163
Brian <blee29@ucsc.edu>
2015-03-02 21:30:40 -0800

    Uploaded bubble sort, heap sort, insertion sort, and merge sort.

---
+++ insertion_sort.hs
@@ -0,0 +1,9 @@
+insertion_sort :: (a -> a -> Bool) -> [a] -> [a]
+insertion_sort pred []     = []
+insertion_sort pred (x:xs) = insert pred x (insertion_sort pred xs)
+
+insert :: (a -> a -> Bool) -> a -> [a] -> [a]
+insert pred x [] = [x]
+insert pred x (y:ys)
+  | pred x y = (x:y:ys)
+  | otherwise = y:(insert pred x ys)
