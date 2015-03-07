25c25245b1941d6ccfa9ed3f0f5e23e53fca02dd
Laptopmini <laptopmini@gmail.com>
2015-03-06 23:45:41 -0800

    Create a Test File Generator
    Usage:
    python make_tfile.py [number of words] [length of each word]

---
+++ make_tfile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# # # # # # # # 
+# MAKE_TFILE  #
+# # # # # # # # 
+
+import sys
+from random import randint
+ 
+def random_word(n):
+    result = ""
+    for i in range(0,int(n)):
+        result = result + str(randint(0,9))
+    return result
+
+def random_gen(new_file, number, dimension):
+    for i in range(0,int(number)):
+        new_file.write(random_word(dimension) + " ")
+
+if (len(sys.argv) > 2):
+    new_file = open("input.txt", "wb")
+    random_gen(new_file, sys.argv[1], sys.argv[2])
+    new_file.close()
+else:
+    print "Usage: make_tfile.py [number of words] [length of each word]"
